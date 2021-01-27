ee2aa87a8aef649044272848910111290dc0ff53
MagnoEfren <58579707+MagnoEfren@users.noreply.github.com>
2021-01-27 08:25:43 -0500

    Add files via upload

---
+++ codigo_arduino/codigo_arduino.ino
@@ -0,0 +1,20 @@
+//https://www.youtube.com/c/MagnoEfren
+//Graph of an  analog signal of arduino in python using tkinter and matplotlib
+
+float lectura;
+float voltaje;
+
+void setup() {
+  
+  Serial.begin(9600);
+  pinMode(A0,INPUT);
+}
+
+void loop() {
+  
+  voltaje = ((lectura/1023)*5.5);
+
+  Serial.println(voltaje);
+
+  delay(100);
+}
